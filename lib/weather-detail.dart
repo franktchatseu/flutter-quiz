583d556c035608044e09e12fdcecbb8cd6f6f95a
frank tchatseu <louenkamfrank@gmail.com>
2020-09-03 02:37:41 +0100

    utilisation de api rest. cas pratique: systeme pour affichage de la meteo des villes

---
+++ lib/weather-detail.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+class WeatherDetail extends StatefulWidget {
+  String city;
+  WeatherDetail(this.city);
+
+  @override
+  _WeatherDetailState createState() => _WeatherDetailState();
+}
+
+class _WeatherDetailState extends State<WeatherDetail> {
+
+  var weatherData;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData(widget.city);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
+        title: Text("la météo de la ville:  ${widget.city}"),
+      ),
+      body: (weatherData == null?Center(
+        child: CircularProgressIndicator(),
+      ):
+          ListView.builder(
+            itemCount: weatherData == null ? 0 : weatherData['list'].length,
+              itemBuilder: (context,index){
+                return Card(
+                  color: Colors.deepOrangeAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage('images/${weatherData['list'][index]['weather'][0]['main'].toLowerCase()}.jpeg'),
+                              radius: 20,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                      children: [
+                                      Text("${DateFormat('E-dd/mm/yyyy').format(DateTime.fromMicrosecondsSinceEpoch(weatherData['list'][index]['dt']*1000000)) }",style: TextStyle(fontSize: 22,color: Colors.white),),
+                                      Text(" | ${weatherData['list'][index]['weather'][0]['main'].toLowerCase().toString()}",style: TextStyle(fontSize: 25,color: Colors.black45,fontWeight: FontWeight.bold,),),
+
+                                    ],
+                                  ),
+                                  Text("${DateFormat('HH:mm').format(DateTime.fromMicrosecondsSinceEpoch(weatherData['list'][index]['dt']*1000000)) }",style: TextStyle(fontSize: 18,color: Colors.white),),
+
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        Text("${weatherData['list'][index]['main']['temp'].round()} C",style: TextStyle(fontSize: 22,color: Colors.white,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                  ),
+                );
+              })
+      ));
+  }
+
+  //recuperation des donnnees de la meteo
+  getData(city){
+    String url ="http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=5332519fc155e6fae0d3c625a24a5b09";
+    http.get(url).then((resp){
+      setState(() {
+        this.weatherData = json.decode(resp.body);
+        print(weatherData);
+      });
+    }).catchError((error){
+      print(error);
+    });
+  }
+}
--- lib/weather.dart
+++ lib/weather.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapplication/weather-detail.dart';
 import './quiz.dart';
 
-class Weather extends StatelessWidget {
+class Weather extends StatefulWidget {
+  @override
+  _WeatherState createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  String city = "Weather";
+  TextEditingController textEditingController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Weather',style: TextStyle(fontSize: 20,backgroundColor: Colors.blue),),
+        title: Text(city,style: TextStyle(fontSize: 20,backgroundColor: Colors.blue),),
       ),
       body: Center(
-        child: Text('la meteo à votre disposition',style: TextStyle(fontSize: 22),),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(hintText: 'entrer le nom de la ville'),
+              style: TextStyle(fontSize: 22 ),
+              controller: textEditingController,
+              onChanged: (value){
+                setState(() {
+                  this.city = value;
+                });
+              },
+              //permet de soumettre le formulaire en cliquant sur le button ok du telephone
+              onSubmitted: (value){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>WeatherDetail(city)));
+                textEditingController.text ="";
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: double.infinity,
+                child: RaisedButton(
+                  color: Colors.deepOrangeAccent,
+                  child: Text("Meteo",style: TextStyle(fontSize: 22,color: Colors.white),),
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>WeatherDetail(city)));
+                    textEditingController.text ="";
+                  },
+                ),
+              ),
+            )
+          ],
+        )
       ),
     );
   }
