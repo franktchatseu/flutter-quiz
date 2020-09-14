ca994778155464196284a7879495276533f086f7
frank tchatseu <louenkamfrank@gmail.com>
2020-09-14 11:26:09 +0100

    start chat

--- lib/gallery-detail.dart
+++ lib/gallery-detail.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 class GalleryDetail extends StatefulWidget {
   String city;
   GalleryDetail(this.city);
@@ -9,63 +10,92 @@ class GalleryDetail extends StatefulWidget {
 }
 
 class _GalleryDetailState extends State<GalleryDetail> {
-  var imagesData;
+  List<dynamic> imagesData = [];
+  int current_page = 1;
+  int per_page = 10;
+  int total_page;
+  ScrollController _scrollController = new ScrollController();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getData(widget.city);
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        if (current_page < total_page) {
+          ++current_page;
+          this.getData(widget.city);
+        }
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("images of ${widget.city}"),
-      ),
-      body: (this.imagesData == null?Center(child: CircularProgressIndicator())
-          :ListView.builder(
-          itemCount: this.imagesData == null? 0: this.imagesData.length,
-          itemBuilder: (context,index){
-              return Column(
-                children: [
-                Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(child: Text(this.imagesData[index]["tags"],style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold,color: Colors.white),))
-                  ),
-                ),
-              ),
-                  Container(
-                    child: Card(
-                      color: Colors.red,
-                      child: Image.network(this.imagesData[index]["webformatURL"]),
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.deepOrangeAccent,
-                  )
-                ],
-              );
-
-          }
-        )
-      )
-    );
+        appBar: AppBar(
+          title: Text(
+              "images of ${widget.city}  page ${current_page}/${total_page}"),
+        ),
+        body: (this.imagesData == null
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                controller: this._scrollController,
+                itemCount: this.imagesData == null ? 0 : this.imagesData.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        child: Card(
+                          color: Colors.blue,
+                          child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(
+                                  child: Text(
+                                this.imagesData[index]["tags"],
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ))),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: Card(
+                            color: Colors.red,
+                            child: Image.network(
+                                this.imagesData[index]["webformatURL"]),
+                          ),
+                        ),
+                      ),
+                      Divider(
+                        color: Colors.deepOrangeAccent,
+                        height: 8,
+                      )
+                    ],
+                  );
+                })));
   }
 
-  getData(city){
-    String url ="https://pixabay.com/api/?key=18183860-1983a89c724eae08522fd2668&q=${city}&page=1&per_page=4";
-    http.get(url).then((resp){
+  getData(city) {
+    String url =
+        "https://pixabay.com/api/?key=18183860-1983a89c724eae08522fd2668&q=${city}&page=${current_page}&per_page=${per_page}";
+    http.get(url).then((resp) {
       setState(() {
-        var parseDate = json.decode(resp.body);
-        this.imagesData = parseDate['hits'];
+        var parseData = json.decode(resp.body);
+        this.imagesData.addAll(parseData['hits']);
+        //recuperation du total de page
+        if (parseData["totalHits"] % current_page == 0) {
+          this.total_page = parseData["totalHits"] ~/ per_page;
+        } else {
+          this.total_page = 1 + (parseData["totalHits"] / per_page).floor();
+        }
         print(imagesData);
       });
-    }).catchError((error){
-
-    });
+    }).catchError((error) {});
   }
 }
